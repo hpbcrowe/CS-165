22f79f5ebaca728744e907f8e359eced2c03d217
hpbcrowe <51143614+hpbcrowe@users.noreply.github.com>
2020-12-22 22:57:39 -0700

    Add files via upload

---
+++ check13b/numberList.h
@@ -0,0 +1,49 @@
+/*******************
+ * NumberList class
+ *******************/
+#ifndef NUMBER_LIST_H
+#define NUMBER_LIST_H
+
+#include <iostream>
+
+class NumberList
+{
+private:
+   int size;
+   int *array;
+
+public:
+   NumberList()
+   {
+      size = 0;
+      array = NULL;
+   }
+
+
+   // TODO: Add your constructor and destructor
+   NumberList(int size)
+   {
+      this->size = size;
+	   array = new int[size];
+	   for (int i = 0; i < size; i++)
+	   {
+		   array[i] = 0;
+	   }
+   }
+   NumberList(const NumberList &copy);
+   NumberList &  operator = (const NumberList &copy);
+   ~NumberList()
+   {
+	   delete []array;
+	   array = NULL;
+	   std::cout << "Freeing memory\n";
+   }
+
+   int getNumber(int index) const;
+   void setNumber(int index, int value);
+   
+   void displayList() const;
+
+};
+
+#endif
